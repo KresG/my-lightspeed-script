b0cf7fe26c1150b6e2d0c34049a20ed26bef39e0
KresG <124288246+KresG@users.noreply.github.com>
2024-09-02 23:30:39 +0800

    Update domainDnsChecker.user.js
    testing update link feature

--- C-Series/domainDnsChecker.user.js
+++ C-Series/domainDnsChecker.user.js
@@ -2,7 +2,7 @@
 // @name         Domain DNS Checker
 // @namespace    http://tampermonkey.net/
 // @match        */admin/domains
-// @version      1
+// @version      1.1
 // @author       Kres G - eCom Support
 // @description  Display domain CNAME and A Records using Google DNS
 // @grant        none
