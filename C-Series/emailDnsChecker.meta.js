0bd142a1852bfa01f45e2476892e074c3eeec2ea
KresG <124288246+KresG@users.noreply.github.com>
2025-01-13 21:23:26 +0700

    Update emailDnsChecker.meta.js

--- C-Series/emailDnsChecker.meta.js
+++ C-Series/emailDnsChecker.meta.js
@@ -2,7 +2,7 @@
 // @name         Email DNS Records Checker
 // @namespace    http://tampermonkey.net/
 // @match        */admin/settings/company/email_dns
-// @version      1.1
+// @version      1.2
 // @author       Kres G - eCom Support
 // @description  Display DNS records, check CNAME, DMARC, and DKIM records using Google DNS API
 // @grant        none
